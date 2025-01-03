af548d5e9ed75f65257f71a3dcc13f047dc0f7f5
mahmutessiz <mahmutessiz71@gmail.com>
2025-01-03 12:41:01 +0300

    chore: remove unnecessary comments

--- app.go
+++ app.go
@@ -25,11 +25,6 @@ func (a *App) startup(ctx context.Context) {
 }
 
 func (a *App) RunScript(script string) {
-	// Get the executable directory
-	// Replace with the actual path to your .sh file
-	// script := `
-	// #!/bin/bash
-	// ddcutil setvcp 10 90`
 
 	// Create a command to execute the script
 	cmd := exec.Command("sh", "-c", script)
@@ -49,10 +44,6 @@ func (a *App) RunScript(script string) {
 }
 
 func (a *App) RunScriptIncrement(script string) {
-	// Replace with the actual path to your .sh file
-	// script := `
-	// #!/bin/bash
-	// ddcutil setvcp 10 90`
 
 	// Create a command to execute the script
 	cmd := exec.Command("sh", "-c", script)
@@ -70,23 +61,3 @@ func (a *App) RunScriptIncrement(script string) {
 	// Print the output of the script
 	fmt.Println(out.String())
 }
-
-// func (a *App) RunScriptIncrement() string {
-// 	// Get the executable directory
-// 	ex, err := os.Executable()
-// 	if err != nil {
-// 		return fmt.Sprintf("Error getting executable path: %v", err)
-// 	}
-// 	execPath := filepath.Dir(ex)
-
-// 	// Construct absolute path to script
-// 	scriptPath := filepath.Join(execPath, "assets", "inc.sh")
-
-// 	cmd := exec.Command("sh", scriptPath)
-// 	output, err := cmd.CombinedOutput()
-// 	if err != nil {
-// 		return fmt.Sprintf("Error: %v\nOutput: %s", err, output)
-// 	}
-
-// 	return string(output)
-// }
